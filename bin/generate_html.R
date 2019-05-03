1be71167a01d6fb0227402e5e35bc720c2061761
Hannah Pliner <hpliner@gmail.com>
2019-05-03 13:41:12 -0700

    initial dmux commit

---
+++ bin/generate_html.R
@@ -0,0 +1,325 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library(ggplot2)
+  library(argparse)
+  library(jsonlite)
+  library(shiny)
+  library(stringr)
+})
+
+parser = argparse::ArgumentParser(description='Script to generate demux dashboard.')
+parser$add_argument('input_folder', help='input folder.')
+parser$add_argument('--p7_rows', required=TRUE, help='p7 rows')
+parser$add_argument('--p5_cols', required=TRUE, help='p5 cols')
+args = parser$parse_args()
+
+
+lane_list <- gsub(".stats.json", "", list.files(args$input_folder, pattern = ".json"))
+lane_names <- gsub("L00", "Lane ", lane_list)
+lane_nums <- gsub("L00", "", lane_list)
+
+p7_rows <- unlist(stringr::str_split(args$p7_rows, " "))
+p5_cols <- unlist(stringr::str_split(args$p5_cols, " "))
+
+
+for (lane in lane_list) {
+  rows <- c("A", "B", "C", "D", "E", "F", "G", "H")
+  cols <- 1:12
+  plate <- data.frame(expand.grid(cols, rows))
+  levels(plate$Var2) <- rev(levels(plate$Var2))
+  
+  rt_counts <- read.csv(paste0(lane, ".rt_counts.csv"), header = F, stringsAsFactors = FALSE)
+  rt_counts$plate <- stringr::str_split_fixed(rt_counts$V1, "-", 2)[,1]
+  rt_counts$well <- stringr::str_split_fixed(rt_counts$V1, "-", 2)[,2] 
+  for(p in unique(rt_counts$plate)) {
+    sub_df <- rt_counts[rt_counts$plate == p,]
+    rt_df <- data.frame(rows = substring(sub_df$well, first = 1, last = 1),
+                        cols = as.numeric(substring(sub_df$well, first = 2,
+                                                    last = nchar(sub_df$well))), ReadCount = sub_df$V2)
+    
+    
+    data <- merge(plate, rt_df, by.x=c("Var1", "Var2"),
+                  by.y=c("cols", "rows"), all.x=T)
+    data$ReadCount[is.na(data$ReadCount)] <- 0
+    
+    png(file = paste0("demux_dash/img/", lane, "_", p, ".rt_plate.png"), width = 6, height = 4, res = 200, units = "in")
+    print(ggplot(aes(as.factor(Var1), Var2, fill = ReadCount), data = data) +
+            geom_point(shape=21, size = 10) + theme_bw() + labs(x = "", y = "") +
+            scale_fill_gradient(low = "white", high = "blue"))
+    dev.off()
+  }
+
+  pcr_counts <- read.csv(paste0(lane, ".pcr_counts.csv"), header = F, stringsAsFactors = FALSE)
+  pcr_counts$p5_row <- substring(pcr_counts$V1, first = 1, last = 1)
+  pcr_counts$p5_col <- as.numeric(substring(pcr_counts$V1, first = 2,
+                                            last = nchar(pcr_counts$V1)))
+
+  pcr_counts$p7_row <- substring(pcr_counts$V2, first = 1, last = 1)
+  pcr_counts$p7_col <- as.numeric(substring(pcr_counts$V2, first = 2,
+                                            last = nchar(pcr_counts$V2)))  
+  for (i in  1:length(unlist(stringr::str_split(args$p7_rows, " ")))) {
+    sub <- subset(pcr_counts, p7_row == p7_rows[i] & p5_col == as.numeric(p5_cols[i]))
+    p5_df <- data.frame(rows = sub$p5_row,
+                        cols = sub$p7_col, ReadCount = sub$V3)
+    data <- merge(plate, p5_df, by.x=c("Var1", "Var2"),
+                  by.y=c("cols", "rows"), all.x=T)
+    data$ReadCount[is.na(data$ReadCount)] <- 0
+    
+    png(file = paste0("demux_dash/img/", lane,"_", p7_rows[i], p5_cols[i], ".pcr_plate.png"), width = 6, height = 4, res = 200, units = "in")
+    print(ggplot(aes(as.factor(Var1), Var2, fill = ReadCount), data = data) +
+            geom_point(shape=21, size = 10) + theme_bw() + labs(x = "", y = "") +
+            scale_fill_gradient(low = "white", high = "blue"))
+    dev.off()
+  }
+  p5_agg <- aggregate(pcr_counts$V3, by = list(orig=pcr_counts$V1), sum)
+  p5_df <- data.frame(rows = substring(p5_agg$orig, first = 1, last = 1),
+                         cols = as.numeric(substring(p5_agg$orig, first = 2,
+                                          last = nchar(p5_agg$orig))), ReadCount = p5_agg$x)
+  
+  data <- merge(plate, p5_df, by.x=c("Var1", "Var2"),
+                by.y=c("cols", "rows"), all.x=T)
+  data$ReadCount[is.na(data$ReadCount)] <- 0
+  
+
+  
+  
+  
+  p7_agg <- aggregate(pcr_counts$V3, by = list(orig=pcr_counts$V2), sum)
+  p7_df <- data.frame(rows = substring(p7_agg$orig, first = 1, last = 1),
+                      cols = as.numeric(substring(p7_agg$orig, first = 2,
+                                       last = nchar(p7_agg$orig))), ReadCount = p7_agg$x)
+  
+  data <- merge(plate, p7_df, by.x=c("Var1", "Var2"),
+                by.y=c("cols", "rows"), all.x=T)
+  data$ReadCount[is.na(data$ReadCount)] <- 0
+  
+  png(file = paste0("demux_dash/img/", lane, ".p7_plate.png"), width = 6, height = 4, res = 200, units = "in")
+  print(ggplot(aes(as.factor(Var1), Var2, fill = ReadCount), data = data) +
+    geom_point(shape=21, size = 10) + theme_bw() + labs(x = "", y = "") +
+    scale_fill_gradient(low = "white", high = "blue") )
+  dev.off()
+
+}
+
+
+
+
+outtab <- lapply(lane_list, function(x) {
+  sumstats <- jsonlite::fromJSON(paste0(x, ".stats.json"))
+  data.frame(`Total input reads` = round(sumstats$total_input_reads),
+             `Total passed reads` = round(sumstats$total_passed_reads),
+             `Pass percentage` = round(sumstats$fraction_passed_reads * 100, digits = 2),
+             `Percent uncorrected`= round(sumstats$fraction_uncorrected_reads * 100, digits = 2),
+             `Percent invalid RT`= round(sumstats$fraction_invalid_rt_well * 100, digits = 2),
+             `Percent PCR mismatch`= round(sumstats$fraction_pcr_mismatch * 100, digits = 2)
+             )
+})
+tab <- as.data.frame(do.call(rbind, outtab))
+row.names(tab) <- lane_names
+
+top <- HTML('<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .navbar-brand
+        {
+          font-size: xx-large !important;
+          display: flex;
+          align-items: center;
+          text-align: center;
+        }
+        .sidebar {
+          position: fixed;
+          top: 0;
+          bottom: 0;
+          left: 10px;
+          z-index: 100; /* Behind the navbar */
+          padding: 100px 0 0; /* Height of navbar */
+          box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+        }
+        </style>
+  </head>')
+
+body <- tags$body(
+  list(
+  HTML('    <nav class="navbar navbar-expand-md sticky-top navbar-light" style="background-color: #e3f2fd;">
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul class="navbar-nav mr-auto">
+                <img src="img/bbi_icon.png" height="70" class="d-inline-block align-top" alt="">
+            </ul>
+        </div>
+        <div class="mx-auto order-0">
+            <a class="navbar-brand mx-auto" href="#">Demultiplexing QC Dashboard</a>
+        </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        </div>
+    </nav>
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#summary">
+                  <span data-feather="home"></span>
+                  Summary Statistics <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#rt">
+                  <span data-feather="file"></span>
+                  RT Barcodes
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#pcr">
+                  <span data-feather="shopping-cart"></span>
+                  PCR Barcodes
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+       <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" style="padding-top: 15px;">'),
+  
+  tags$div(class="tab-content", id="nav-tabContent",
+    list(HTML('<div class="tab-content" id="nav-tabContent">
+              <div class="tab-pane fade show active" id="navstat-lane1" role="tabpanel" aria-labelledby="navstat-lane1-tab">
+                  <h3 class="h3" id="summary">Summary statistics</h3>
+                  <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th scope="col"></th>'), 
+    lapply(lane_names, function(lane) {
+      tags$th(scope="col", lane)
+    }),
+    HTML('</tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">Total input reads</th>'),
+    lapply(tab$Total.input.reads, function(lane) {
+      tags$td(lane)
+    }),
+    HTML('</tr>
+                        <tr>
+                          <th scope="row">Total passed reads</th>'),
+    lapply(tab$Total.passed.reads, function(lane) {
+      tags$td(lane)
+    }),   
+    HTML('                        </tr>
+                        <tr>
+                          <th scope="row">Pass percentage</th>'),
+    lapply(tab$Pass.percentage, function(lane) {
+      tags$td(lane)
+    }),
+    HTML('                        </tr>
+                        <tr>
+                          <th scope="row">Percent uncorrected</th>'),
+    lapply(tab$Percent.uncorrected, function(lane) {
+      tags$td(lane)
+    }),
+    HTML('                        </tr>
+                        <tr>
+                          <th scope="row">Percent invalid RT</th>'),
+    lapply(tab$Percent.invalid.RT, function(lane) {
+      tags$td(lane)
+    }),
+    HTML('                        </tr>
+                        <tr>
+                          <th scope="row">Percent PCR mismatch</th>'),
+    lapply(tab$Percent.PCR.mismatch, function(lane) {
+      tags$td(lane)
+    }),
+    HTML('                       </tr>
+                      </tbody>
+                    </table>'))),
+  HTML('          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h3" id="pcr">RT Barcodes</h1>
+            </div>
+          <nav>
+              <div class="nav nav-tabs" id="navrt-tab" role="tablist">'),
+  lapply(lane_nums, function(num) {
+    tags$a(class="nav-item nav-link", id=paste0("navrt-lane", num, "-tab"), 
+           `data-toggle`="tab", href=paste0("#navrt-lane", num), role="tab", 
+           `aria-controls`=paste0("navrt-lane", num),`aria-selected`="false", paste("Lane", num))
+  }),
+  HTML('              </div>
+          </nav>
+       <div class="tab-content" id="nav-tabContent">'),
+  lapply(lane_nums, function(num) {
+    tags$div(class="tab-pane fade", id=paste0("navrt-lane", num), 
+             role="tabpanel", `aria-labelledby`=paste0("navrt-lane", num, "-tab"),
+             list(lapply(unique(rt_counts$plate), function(p) {
+               list(tags$h4(paste0("Plate ",p)),
+                    tags$img(src=paste0("img/L00", num, "_", p, ".rt_plate.png"), 
+                             width = "50%",
+                             class="rounded mx-auto d-block",
+                             alt="..."))
+             })))
+
+  }),
+ 
+  # PCR barcodes
+  
+  HTML('          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h3" id="pcr">PCR Barcodes</h1>
+            </div>
+          <nav>
+              <div class="nav nav-tabs" id="navrt-tab" role="tablist">'),
+  lapply(lane_nums, function(num) {
+    tags$a(class="nav-item nav-link", id=paste0("navpcr-lane", num, "-tab"), 
+           `data-toggle`="tab", href=paste0("#navpcr-lane", num), role="tab", 
+           `aria-controls`=paste0("navpcr-lane", num),`aria-selected`="false", paste("Lane", num))
+  }),
+  HTML('              </div>
+          </nav>
+       <div class="tab-content" id="nav-tabContent">'),
+  lapply(lane_nums, function(num) {
+    tags$div(class="tab-pane fade", id=paste0("navpcr-lane", num), 
+             role="tabpanel", `aria-labelledby`=paste0("navpcr-lane", num, "-tab"),
+             list(lapply(1:length(unlist(stringr::str_split(args$p7_rows, " "))), function(i) {
+               list(tags$h4(paste0("PCR Combo ",p7_rows[i], p5_cols[i])),
+                    tags$img(src=paste0("img/L00", num, "_", p7_rows[i], p5_cols[i], ".pcr_plate.png"), 
+                             width = "50%",
+                             class="rounded mx-auto d-block",
+                             alt="..."))
+             })))
+    
+  }),
+  
+  
+  
+   
+    HTML('
+    </main>
+      </div>
+      </div>
+      
+      
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      ')
+  )
+)
+
+end <- HTML('</html>')
+  
+
+fileConn<-file("demux_dash/demux_dash.html")
+writeLines(c(as.character(top),as.character(body), as.character(end)), fileConn)
+close(fileConn)
+
+
+
+
---
+++ main.nf
@@ -0,0 +1,176 @@
+
+// Parse input parameters
+params.help = false
+params.rerun = false
+
+//print usage
+if (params.help) {
+    log.info ''
+    log.info 'BBI 2-level sci-RNA-seq Demultiplexer'
+    log.info '--------------------------------'
+    log.info ''
+    log.info 'For reproducibility, please specify all parameters to a config file'
+    log.info 'by specifying -c CONFIG_FILE.config.'
+    log.info ''
+    log.info 'Usage: '
+    log.info '    nextflow run bbi-dmux -c CONFIG_FILE'
+    log.info ''
+    log.info 'Help: '
+    log.info '    --help                              Show this message and exit.'
+    log.info ''
+    log.info 'Required parameters (specify in your config file):'
+    log.info '    params.run_dir = RUN_DIRECTORY             Path to the sequencer output.'
+    log.info '    params.output_dir OUTPUT DIRECTORY         Output directory.'
+    log.info '    params.sample_sheet = SAMPLE_SHEET_PATH    Sample sheet of the format described in the README.'
+    log.info '    params.p7_rows = "A B C"                   The PCR rows used - must match order of params.p5_cols.'
+    log.info '    params.p5_cols = "1 2 3"                   The PCR columns used - must match order of params.p7_rows.'
+    log.info ''
+    log.info ''
+    log.info 'Optional parameters (specify in your config file):'
+    log.info '    params.max_cores = 16                      The maximum number of cores to use - fewer will be used if appropriate.'
+    log.info '    process.maxForks = 20                      The maximum number of processes to run at the same time on the cluster.'
+    log.info '    process.queue = "trapnell-short.q"         The queue on the cluster where the jobs should be submitted. '
+    log.info '    params.rerun = [sample1, sample2]          Add to only rerun certain samples from trimming on.'
+    log.info ''
+    log.info 'Issues? Contact hpliner@uw.edu'
+    exit 1
+}
+
+// check required options
+if (!params.run_dir || !params.output_dir || !params.sample_sheet || !params.p7_rows || !params.p5_cols) {
+    exit 1, "Must include config file using -c CONFIG_FILE.config that includes output_dir, sample_sheet, run_dir, p7_rows and p5_cols"
+}
+
+//rt2_file = Channel.fromPath('barcode_files/rt2.txt')
+//check sample sheet
+process check_sample_sheet {
+    module 'modules:java/latest:modules-init:modules-gs:python/3.6.4'
+
+    input:
+	val params.sample_sheet
+
+    output:
+        file "*.csv" into good_sample_sheet
+
+    """
+    check_sample_sheet.py --sample_sheet $params.sample_sheet
+    """
+}
+
+sample_sheet_file = good_sample_sheet
+
+process make_sample_sheet {
+    cache 'lenient'
+    module 'java/latest:modules:modules-init:modules-gs:python/3.6.4'
+
+    input:
+        val params.run_dir
+        file good_sample_sheet
+
+    output:
+        file "SampleSheet.csv" into samp_sheet
+
+    """
+    make_sample_sheet.py --run_directory $params.run_dir
+
+    """    
+}
+
+if (params.max_cores > 16) {
+    max_cores_bcl = 16
+    bcl_mem = 2.5
+} else {
+    max_cores_bcl = params.max_cores
+    bcl_mem = 40/max_cores_bcl
+}
+
+process bcl2fastq {
+    cache 'lenient'
+    module 'java/latest:modules:modules-init:modules-gs:gmp/5.0.2'
+    module 'mpfr/3.1.0:mpc/0.8.2:gcc/4.9.1:bcl2fastq/2.20'
+    publishDir path: "$params.output_dir", pattern: "lane_fastqs/Undetermined_S0_*.fastq.gz", mode: 'copy'
+    clusterOptions "-pe serial $max_cores_bcl -l mfree=$bcl_mem" + "G"
+
+    input:
+        file samp_sheet
+
+    output:
+        file "lane_fastqs" into bcl2fastq_output
+        set file("lane_fastqs/Undetermined_S0_*_R1_001.fastq.gz"), file("lane_fastqs/Undetermined_S0_*_R2_001.fastq.gz") into fastqs mode flatten
+        file "lane_fastqs/fake*.gz" into fakes mode flatten
+
+    """
+    min_threads=\$((($max_cores_bcl/2)<4 ? ($max_cores_bcl/2):4))
+
+    bcl2fastq -R $params.run_dir --output-dir ./lane_fastqs \
+        --sample-sheet $samp_sheet \
+        --loading-threads \$min_threads \
+        --processing-threads $max_cores_bcl  \
+        --writing-threads \$min_threads \
+        --barcode-mismatches 1 \
+        --ignore-missing-positions \
+        --ignore-missing-controls \
+        --ignore-missing-filter \
+        --ignore-missing-bcls \
+        --minimum-trimmed-read-length 15 \
+        --mask-short-adapter-reads 15
+
+    """
+}
+
+process seg_sample_fastqs {
+    cache 'lenient'
+    module 'java/latest:modules:modules-init:modules-gs:python/3.6.4'
+    clusterOptions "-l mfree=1G"
+    publishDir = [path: "${params.output_dir}/", pattern: "demux_stats/*.stats.json", mode: 'copy']
+    publishDir = [path: "${params.output_dir}/sample_fastqs", pattern: "demux_stats/*.fastq", mode: 'copy']
+    publishDir = [path: "${params.output_dir}/", pattern: "demux_stats/*.csv", mode: 'copy'] 
+
+    input:
+        set file(R1), file(R2) from fastqs
+        file sample_sheet_file
+
+    output:
+        file "demux_stats/*" into seg_output
+        file "demux_stats/*.fastq" into samp_fastqs_check mode flatten
+        file "demux_stats/*.stats.json" into stats
+        file "demux_stats/*.csv" into csv_stats
+
+    """
+    mkdir demux_stats
+    mkdir sample_fastqs
+    make_sample_fastqs.py --run_directory $params.run_dir \
+        --read1 <(zcat $R1) --read2 <(zcat $R2) \
+        --file_name $R1 --sample_layout $sample_sheet_file \
+        --p5_cols_used $params.p5_cols --p7_rows_used $params.p7_rows \
+        --output_dir ./demux_stats
+
+    """    
+}
+
+
+process demux_dash {
+    module 'java/latest:modules:modules-init:modules-gs:gcc/8.1.0:R/3.5.2'
+    clusterOptions "-l mfree=8G"
+
+    publishDir path: "${params.output_dir}/", pattern: "demux_dash", mode: 'copy'
+
+
+    input:
+        file demux_stats_files from seg_output.collect()
+        file icon from Channel.fromPath('./bin/bbi_icon.png')
+    output:
+        file demux_dash
+
+    """
+    mkdir demux_dash
+    mkdir demux_dash/img
+    cp $icon demux_dash/img/
+    generate_html.R \
+        "." --p7_rows "$params.p7_rows" --p5_cols "$params.p5_cols"
+
+    """
+
+
+
+}

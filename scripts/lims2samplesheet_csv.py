25bd2c05291486574856822de50cef45df44f57c
brgew <bge@uw.edu>
2023-11-03 09:29:41 -0700

    Fix lims2samplesheet_csv.py to make full spreadsheet style samplesheet.

--- scripts/lims2samplesheet_csv.py
+++ scripts/lims2samplesheet_csv.py
@@ -23,8 +23,11 @@ def check_header(row):
      row[2] != 'plate' or
      row[3] != 'coordinates' or
      row[4] != 'specimen' or
-     row[5] != 'organism' or
-     row[6] != 'genome'):
+     row[5] != 'investigatorSpecimenId' or
+     row[6] != 'investigator' or
+     row[7] != 'organism' or
+     row[8] != 'tissue' or
+     row[9] != 'genome'):
     print('Error: unexpected header token in row: ', row, file=sys.stderr)
     sys.exit(-1)
 
@@ -58,12 +61,25 @@ def format_rows(inrows):
     if(mobj2 == None):
       print('Error: no match to well \'%s\'' % (inrow[3]), file=sys.stderr)
       sys.exit(-1)
-#    plate_str  = inrow[2]
-    plate_str  = 'P' + '%d' % (int(mobj1.group(1)))
-    well_str   = '%s%02d' % (mobj2.group(1), int(mobj2.group(2)))
-    sample_str = inrow[4]
-    genome_str = inrow[6]
-    outrow = ['%s%s' % (plate_str, well_str), '%s-%s' % (plate_str, well_str), sample_str, genome_str]
+
+#    plate_str              = inrow[2]
+    plate_str              = 'P' + '%d' % (int(mobj1.group(1)))
+    well_str               = '%s%02d' % (mobj2.group(1), int(mobj2.group(2)))
+    sample_str             = inrow[4]
+    investigator_sample_id = inrow[5]
+    tstr                   = inrow[6].split(':')
+    if(len(tstr) > 2):
+      print('More than one semicolon in investigator string \'%s\'' % (inrow[6]))
+    investigator_str       = inrow[6].split(':')[0]
+    tissue_str             = inrow[8]
+    genome_str             = inrow[9]
+    outrow = ['%s%s' % (plate_str, well_str), sample_str, investigator_str, genome_str, tissue_str, '%s-%s' % (plate_str, well_str), investigator_sample_id]
+#   0    sample
+#   1    investigator
+#   2    genome
+#   3    tissue
+#   4    plate-well
+#   5    samples
     outrows.append(outrow)
   return(outrows)
 
@@ -72,16 +88,16 @@ def format_rows(inrows):
 def write_sample_sheet(outrows, outfile):
   with open(outfile, 'w+', newline='') as ofp:
     writer = csv.writer(ofp, dialect='unix', quoting=csv.QUOTE_NONE)
-    writer.writerow(['RT Barcode','Sample ID','Reference Genome'])
+#    writer.writerow(['RT Barcode','Sample ID','Reference Genome'])
     for outrow in outrows:
-      writer.writerow([outrow[1], outrow[2], outrow[3]])
+      writer.writerow([outrow[1], outrow[2], outrow[3], outrow[4], outrow[5], outrow[6]])
 
 
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser(description='A program to make a samplesheet CSV file for the bbi-dmux pipeline from a LIMS CSV experiment manifest file.')
   parser.add_argument('-i', '--in_file', required=True, help='Input LIMS manifest CSV filename.')
-  parser.add_argument('-o', '--out_file', required=False, default='SampleSheet.csv', help='Output CSV filename (default is SampleSheet.csv).')
+  parser.add_argument('-o', '--out_file', required=False, default='samplesheet_spreadsheet.csv', help='Output CSV filename (default is SampleSheet.csv).')
   args = parser.parse_args()
 
   infile = args.in_file
